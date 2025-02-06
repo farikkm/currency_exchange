7397554a052f1d23b549c654d9333a2670672b3a
farikkm <farikk.v.2@gmail.com>
2025-02-06 21:55:51 +0500

    Header is updated: Navigation features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,41 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="header d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a
         href="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        style={{
+          display: 'flex',
+          gap: '10px',
+          alignItems: 'center'
+        }}
       >
+        <img style={{
+          width: '50px'
+        }} src="/logo.png" alt="" />
         <span className="fs-4">Currency Exchanger</span>
       </a>
 
       <ul className="nav nav-pills">
         <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+          <NavLink
+            to="/"
+            className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+            aria-current="page"
+          >
             Home
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="#" className="nav-link">
+          <NavLink
+            to="/exchange"
+            className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`}
+            aria-current="page"
+          >
             Currency Exchange
-          </a>
+          </NavLink>
         </li>
       </ul>
     </header>
